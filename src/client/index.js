f3a746d296cd0949470bfa02cb11af6f6ca841fb
vsk106 <vsk106@gmail.com>
2020-02-13 20:38:33 +0000

    adding gitignore

--- src/client/index.js
+++ src/client/index.js
@@ -2,27 +2,27 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 class App extends Component {
-    state = {
-      count: 10
-    };
+  state = {
+    count: 10
+  };
 
-    increment = () => {
-      const currentVal  = this.state.count;
-      this.setState({ count: currentVal + 1});
-    }
+  increment = () => {
+    const currentVal = this.state.count;
+    this.setState({ count: currentVal + 1 });
+  }
 
-    decrement = () => {
-      const currentVal  = this.state.count;
-      this.setState({count: currentVal - 1});
-    }
+  decrement = () => {
+    const currentVal = this.state.count;
+    this.setState({ count: currentVal - 1 });
+  }
   render() {
-     return (
-       <div>
-         <h1>{this.state.count}</h1>
-         <button onClick={this.increment}>+</button>
-         <button onClick={this.decrement}>-</button>
-       </div>
-     );
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+      </div>
+    );
   }
 }
 
